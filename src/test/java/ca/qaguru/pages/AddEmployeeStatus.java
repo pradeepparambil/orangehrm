7b8de2e378e32f9ed20e4a5536ea9f1958993a9e
sreejithsipvr <sreejithsipvr@gmail.com>
2020-11-12 21:04:31 -0500

    Admin->Job->EmploymentStatus section delete employmentstatus test completed.

--- src/test/java/ca/qaguru/pages/AddEmployeeStatus.java
+++ src/test/java/ca/qaguru/pages/AddEmployeeStatus.java
@@ -4,6 +4,7 @@ import ca.qaguru.lib.PageBase;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
 
 public class AddEmployeeStatus extends PageBase {
     public AddEmployeeStatus(WebDriver driver) {
@@ -12,9 +13,24 @@ public class AddEmployeeStatus extends PageBase {
     private String btnAdd="//*[@name='btnAdd']";
     private String txtName="//*[@id='empStatus_name']";
     private String btnSave="//*[@id='btnSave']";
+    private String chkStatus="//*[text()='Full-Time Contract']//parent::td[@class='left']//preceding-sibling::td//input[@id='ohrmList_chkSelectRecord_3']";
+    private String btnDelete="//input[@id='btnDelete']";
+    private String btnOk="//*[@id='dialogDeleteBtn']";
     public void addStatus(String name){
         click(By.xpath(btnAdd));
         setText(By.xpath(txtName),name);
         click(By.xpath(btnSave));
+    }
+    public void deleteStatus(){
+        click(By.xpath(chkStatus));
+        //if(chkStatus.equals(true)){
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        click(By.xpath(btnDelete));
+        click(By.xpath(btnOk));
+        //}
     }
 }
--- src/test/java/ca/qaguru/tests/EmploymentStatusTest.java
+++ src/test/java/ca/qaguru/tests/EmploymentStatusTest.java
@@ -6,13 +6,22 @@ import ca.qaguru.pages.LoginPage;
 import org.testng.annotations.Test;
 
 public class EmploymentStatusTest extends TestBase {
-    @Test
+    @Test(priority = 1)
     public void addEmploymentStatus(){
         new LoginPage(driver)
                 .login("Admin","admin123")
                 .selectMenu("Admin|Job|Employment Status");
         new AddEmployeeStatus(driver)
                 .addStatus("Full-Time Internship");
+
+    }
+    @Test(priority = 2)
+    public void deleteEmploymentStatus(){
+        new LoginPage(driver)
+                .login("Admin","admin123")
+                .selectMenu("Admin|Job|Employment Status");
+        new AddEmployeeStatus(driver)
+                .deleteStatus();
 
     }
 }
